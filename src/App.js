cd9dc59c925583ace74ebe87bb087317812b072c
Shubham Raj <shubham14p3@gmail.com>
2020-08-05 17:18:19 +0530

    reinitialised the folder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+//import ReactDOM from 'react-dom'
+import Counter from "./component/Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mount: true,
+    };
+
+    this.mountCounter = () => this.setState({mount: true});
+    this.unmountCounter = () => this.setState({mount: false});
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.mountCounter} disabled={this.state.mount}>
+          Mount
+        </button>
+        <button onClick={this.unmountCounter} disabled={!this.state.mount}>
+          Unmount
+        </button>
+        {this.state.mount ? <Counter /> : null}
+      </div>
+    );
+  }
 }
 
 export default App;
